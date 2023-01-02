d4a6001d7f7dd51f5b47d94689db81d6662dd399
lbackman <lucasbackman@gmail.com>
2023-01-02 17:00:54 +0200

    Update EventsController UsersController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,28 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.events.build(event_params)
+  
+    if @event.save
+      redirect_to root_path, notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:date)
+    end
 end
