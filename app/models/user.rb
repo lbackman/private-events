a0dc38bfa2712159b647faa9029710fff77966d9
lbackman <lucasbackman@gmail.com>
2023-01-03 17:51:48 +0200

    Require username for signup
    Username must be unique and at least 4 characters
    Email must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable
 
   has_many :events, foreign_key: 'creator_id'
+
+  validates :username, uniqueness: true, length: { minimum: 4 }
+  validates :email, uniqueness: true
 end
