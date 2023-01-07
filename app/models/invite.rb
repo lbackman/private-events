8d8cd5659480e41870220c20d4823513d6dec059
lbackman <lucasbackman@gmail.com>
2023-01-07 16:23:15 +0200

    Validate uniqueness on id directly
    And change error messsage.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,5 +2,5 @@ class Invite < ApplicationRecord
   belongs_to :event
   belongs_to :attendee, class_name: 'User'
 
-  validates :event, uniqueness: { scope: :attendee, message: "You can't invite a user more than once."}
+  validates :event_id, uniqueness: { scope: :attendee_id, message: "can't have more than one invite per attendee."}
 end
