6d270ce0d35c04ce32611d176024d75001200eb5
lbackman <lucasbackman@gmail.com>
2023-01-08 11:01:54 +0200

    Separate past and upcoming events

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,9 +1,17 @@
 module DateHelper
-  def date_informal(date)
+  def date_time_informal(date)
     if date.year == Time.now.year
       date.strftime("%A %b %-d, %H:%M")
     else
       date.strftime("%A %b %-d %Y, %H:%M")
+    end
+  end
+
+  def date_informal(date)
+    if date.year == Time.now.year
+      date.strftime("%A %b %-d")
+    else
+      date.strftime("%A %b %-d %Y")
     end
   end
 end
