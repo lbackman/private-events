a57caea20b70afee3c2c7263610972a1e3d0cf71
lbackman <lucasbackman@gmail.com>
2023-01-08 09:36:25 +0200

    Prevent user from creating event in the past

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :invites, dependent: :destroy
   has_many :attendees, through: :invites, source: :attendee
+
+  validates :date, comparison: { greater_than: Time.now, message: "of the event must be in the future!" }
 end
