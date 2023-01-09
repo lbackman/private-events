b896d661f299161f9b888adb97fbae252303ef5c
lbackman <lucasbackman@gmail.com>
2023-01-09 08:50:54 +0200

    Add class method for checking event time

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,12 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invites, source: :attendee
 
   validates :date, comparison: { greater_than: Time.now, message: "of the event must be in the future!" }
+
+  def self.past
+    where("date < ?", Time.now).order(:date)
+  end
+
+  def self.future
+    where("date > ?", Time.now).order(:date)
+  end
 end
