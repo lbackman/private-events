7bc108f6f25f81b88bc101507c82606250dcfe5e
lbackman <lucasbackman@gmail.com>
2023-01-09 10:33:42 +0200

    Create new scopes and move view to partial

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,4 +3,12 @@ class Invite < ApplicationRecord
   belongs_to :attendee, class_name: 'User'
 
   validates :event_id, uniqueness: { scope: :attendee_id, message: "can't have more than one invite per attendee."}
+
+  def self.accepted(event)
+    where(event_id: event.id, accepted: true)
+  end
+
+  def self.pending(event)
+    where(event_id: event.id, accepted: nil)
+  end
 end
